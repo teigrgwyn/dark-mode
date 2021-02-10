f64207086f6908180f7d3a0e455fc04c2255b281
Cory Young <teigrgwyn@gmail.com>
2021-02-10 16:00:10 -0600

    useLocalStorage hook added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from 'react';
 
+export const useDarkLocalStorage = (initialValue) => {
+  // for the defaults, use existing value if it exists, otherwise use a new setting as default
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem('darkToggle');
+    console.log("useDarkLocalStorage Set: " + item);
+    return item ? JSON.parse(item) : initialValue;
+  })
+
+  // code to actually change the state value & local storage value
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem('darkToggle', JSON.stringify(value));
+  }
+
+  return [storedValue, setValue];
+}
+
 const Navbar = (props) => {
   const toggleMode = e => {
     e.preventDefault();
     props.setDarkMode(!props.darkMode);
+    
+    console.log("useDarkLocalStorage Changed To: " + window.localStorage.darkToggle);
   };
   return (
     <nav className="navbar">
